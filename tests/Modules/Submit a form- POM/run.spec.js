13e11e8d5b72842f497adbb0b7a3c2cea7d172c7
Damian Bravi <damianbravi@gmail.com>
2024-02-19 11:36:21 -0300

    add changes

--- tests/Modules/Submit a form- POM/run.spec.js
+++ tests/Modules/Submit a form- POM/run.spec.js
@@ -1,23 +1,23 @@
-import { test, expect } from '@playwright/test';
+import { test } from '@playwright/test';
 import { basePage } from './page';
 import { formData1, formData2 } from './data.json';
 
 // Using Mocha-style describe block
 test.describe('Test Suite 01', () => {
+  let basepage;
+
   test.beforeEach(async ({ page }) => {
-    const basepage = new basePage(page);
+    basepage = new basePage(page);
     await basepage.navigate();
   });
 
-  test.only('testCase 01', async ({ page }, testInfo) => {
-    const basepage = new basePage(page);
+  test.only('testCase 01', async ({}, testInfo) => {
     await basepage.fillForm(formData1);
     await basepage.validateForm();
     await basepage.captureAndAttachScreenshot(testInfo, 'Evidence');
   });
   
-  test('testCase 02', async ({ page }) => {
-    const basepage = new basePage(page);
+  test('testCase 02', async ({}, testInfo) => {
     await basepage.fillForm(formData2);
     await basepage.validateForm();
     await basepage.captureAndAttachScreenshot(testInfo, 'Evidence');
