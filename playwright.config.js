e50928d6174fc024ebea57f733581da74a576259
Damian Bravi <damianbravi@gmail.com>
2024-02-24 18:24:35 -0300

    adding reporter

--- playwright.config.js
+++ playwright.config.js
@@ -22,7 +22,7 @@ module.exports = defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['list'], ['html']],
+  reporter: [['list'], ['html'], ['@estruyf/github-actions-reporter']],
 
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
