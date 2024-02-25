274261846889fc60a738cd546e827d8b5d0c07a2
Damian Bravi <damianbravi@gmail.com>
2024-02-25 12:33:28 -0300

    .

--- playwright.config.js
+++ playwright.config.js
@@ -22,7 +22,11 @@ module.exports = defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['list'], ['html'], ['@estruyf/github-actions-reporter']],
+  reporter: [['list'], ['html'], ['@estruyf/github-actions-reporter', {
+    title: 'My custom title',
+    useDetails: true,
+    showError: true
+  }]],
 
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
