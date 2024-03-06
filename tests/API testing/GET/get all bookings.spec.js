d57d47179583124c6b4bafb0eb42af89255450b3
Damian Bravi <damianbravi@gmail.com>
2024-03-06 16:47:27 -0300

    changes

--- tests/API testing/GET/get all bookings.spec.js
+++ tests/API testing/GET/get all bookings.spec.js
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test'
 
 test('should be get all the booking details', async ({ request }) => {
   const response = await request.get('/booking')
-  console.log(await response.json())
+  // console.log(await response.json())
   expect(response.ok()).toBeTruthy()
   expect(response.status()).toBe(200)
 })
